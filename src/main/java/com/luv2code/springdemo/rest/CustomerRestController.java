3f539e437f4a6e27ccb410f2ae14c2b6cfb41597
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-17 07:48:57 +0100

    Add deleteCustomer endPoint.

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -50,4 +50,17 @@ public class CustomerRestController {
         return theCustomer;
     }
 
+    // add mapping for DELETE /customers - update an existing customer
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId) {
+        Customer tempCustomer = customerService.getCustomer(customerId);
+
+        // throw exception if null
+        if (tempCustomer == null) {
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
+
+        customerService.deleteCustomer(customerId);
+        return "Deleted customer id - " + customerId;
+    }
 }
